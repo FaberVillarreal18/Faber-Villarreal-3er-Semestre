85cc480e1fb4c335f8e42a58310ed144ec920844
FaberVillarreal18 <faber.villarrealantequera@unicolombo.edu.co>
2024-09-01 10:09:37 -0500

    Clases terminadas Getters and Setters

---
+++ Ejercicios en clase-POO/Ejercicio 2. Pagos de sueldos/Deduccion.java
@@ -0,0 +1,15 @@
+public class Deduccion {
+    private double porcentaje;
+
+    public Deduccion(double porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+
+    public double getPorcentaje() {
+        return porcentaje;
+    }
+
+    public void setPorcentaje(double porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+}
---
+++ Ejercicios en clase-POO/Ejercicio 2. Pagos de sueldos/Devengo.java
@@ -0,0 +1,27 @@
+public class Devengo {
+    private String persepciones_salariales;
+    private String  persepciones_no_salariales;
+
+    public Devengo(String persepciones_salariales, String persepciones_no_salariales) {
+        this.persepciones_salariales = persepciones_salariales;
+        this.persepciones_no_salariales = persepciones_no_salariales;
+    }
+
+    public String getPersepciones_salariales() {
+        return persepciones_salariales;
+    }
+
+    public void setPersepciones_salariales(String persepciones_salariales) {
+        this.persepciones_salariales = persepciones_salariales;
+    }
+
+    public String getPersepciones_no_salariales() {
+        return persepciones_no_salariales;
+    }
+
+    public void setPersepciones_no_salariales(String persepciones_no_salariales) {
+        this.persepciones_no_salariales = persepciones_no_salariales;
+    }
+    
+}
+
